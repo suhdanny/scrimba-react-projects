94ea293f2ee3ce44d980d9119df00f3d01b4d272
suhdanny <suhdong21@gmail.com>
2022-06-20 23:23:57 -0400

    Implement Navbar Component

--- travel-journal-app/src/App.js
+++ travel-journal-app/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Card from './components/Card';
+import Navbar from './components/Navbar'
+import data from './data';
 
 function App() {
+
+  const cards = data.map(item => <Card key={item.id} {...item}/>)
+
   return (
-    <div>
-        <h1>Hello, World</h1>
+    <div className='main'>
+        <Navbar />
+        {/* {cards} */}
     </div>
   );
 }
---
+++ travel-journal-app/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img alt="Navbar Logo" src="images/globe.png" className="nav--logo"/>
+            <span className="nav--title">my travel journal.</span>
+        </nav>
+    )
+}
