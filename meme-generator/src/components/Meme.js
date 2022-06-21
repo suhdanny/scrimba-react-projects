0eebbbab607f1848dd357c4f341a9bb14353587e
suhdanny <suhdong21@gmail.com>
2022-06-21 14:47:56 -0400

    Implement Meme component with css

---
+++ meme-generator/src/components/Meme.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Meme() {
+    return (
+        <main className='form-container'>
+            <form className='main--form'>
+                <input type='text'/>
+                <input type='text'/>
+                <button className='main--btn'>Get a new meme image  🖼</button>
+            </form>
+        </main>
+    )
+}
