bedaf5b9787648a3cd82854c926efa4bcf745858
suhdanny <suhdong21@gmail.com>
2022-06-30 22:29:09 -0400

    Implement starting page

--- quizzical-app/src/App.js
+++ quizzical-app/src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 
 function App() {
     return (
-        <div>Hello</div>
+        <main>
+            <div className='start-quiz'>
+                <h1 className='quiz-title'>Quizzical</h1>
+                <p className='quiz-description'>Press the button below to start the quiz!</p>
+                <button className='quiz-start'>Start Quiz</button>
+            </div>
+        </main>
     )
 }
 
